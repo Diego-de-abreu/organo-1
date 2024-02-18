fb542742f94379882668fe0286bb26a9efe61280
Diego de Abreu <diego.abreu@artit.com.br>
2024-02-18 19:17:08 -0300

    Curso completo do  React componentes

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,13 +1,33 @@
+import { AiFillCloseCircle } from "react-icons/ai";
+import { FaHeartBroken } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar,aofavoritar }) => {
+    function favoritar(){
+       aofavoritar(colaborador.id);
+    }
+
     return (<div className="colaborador">
+         <AiFillCloseCircle
+            size={25} 
+            className='deletar'
+            onClick={() => aoDeletar(colaborador.id)}
+        /> 
+
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
         <div className="rodape">
             <h4>{colaborador.nome}</h4>
             <h5>{colaborador.cargo}</h5>
+            <div className='favoritar'>
+            {  colaborador.favorito 
+                ? <FaHeart size={25} onClick={favoritar} color="#ff0000"/> 
+                : <FaHeartBroken sizer={25} onClick={favoritar}
+                />
+            }
+            </div>
         </div>
     </div>)
 }
